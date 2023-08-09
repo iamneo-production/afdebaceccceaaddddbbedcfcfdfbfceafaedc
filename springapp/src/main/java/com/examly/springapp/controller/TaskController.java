dd03b810e012b69abca831624998b1ce3b4a006b
Iamneo <techadmin@iamneo.ai>
2023-08-09 05:19:06 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/TaskController.java
+++ springapp/src/main/java/com/examly/springapp/controller/TaskController.java
@@ -1,3 +1,13 @@
 package com.examly.springapp.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
+import com.examly.springapp.controller.TaskRepo;
+import com.examly.springapp.model.Task;
--- springapp/src/main/java/com/examly/springapp/dao/TaskRepo.java
+++ springapp/src/main/java/com/examly/springapp/dao/TaskRepo.java
@@ -1,6 +1,6 @@
 package com.examly.springapp.dao;
 import org.springframework.data.repository.CrudRepository;
-
+import com.examly.springapp.model.Task;
 
 public interface TaskRepo extends CrudRepository<Task,Integer>{
 
